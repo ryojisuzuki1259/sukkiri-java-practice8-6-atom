91eacbb027305501fd8cdba3ada334262c193fb1
鈴木良治 <suzukiryoji@suzukiyoshiharunoMacBook-Air.local>
2021-04-16 04:09:53 +0900

    Answer8-4

--- Cleric.java
+++ Cleric.java
@@ -11,4 +11,16 @@ public class Cleric {
     this.mp -= 5;
     System.out.println("HPが最大まで回復した");
   }
+
+  public int pray(int sec){
+    System.out.println(this.name + "は" + sec + "秒間天に祈った！");
+
+    int recover = new Random().nextInt(3) + sec;
+
+    int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
+
+    this.mp += recoverActual;
+    System.out.println("MPが" + recoverActual + "回復した");
+    return recoverActual;
+  }
 }
